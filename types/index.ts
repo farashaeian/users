e8158b25b38ad9acf14f33df93bd90f26edc887a
farashaeian <z.farashaeian@gmail.com>
2024-10-22 17:42:31 +0330

    put types in seprate file

---
+++ types/index.ts
@@ -0,0 +1,21 @@
+export interface User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    avatar: string;
+}
+
+export interface ApiResponse {
+    data: User[];
+    page: number;
+    total_pages: number;
+}
+export interface UserInfo {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    avatar: string;
+    onClick?: () => void;
+}
